313adb5c92b2f882da59e193d5780206a6cc3efd
Dan Collis-Puro <djcp@thoughtbot.com>
2013-10-11 15:11:57 -0400

    Refactor index.

--- bin/nodeharness.sh
+++ bin/nodeharness.sh
@@ -12,16 +12,20 @@ restart_node (){
 
 restart_node
 
-inotifywait --excludei=bin|\.git -q -m -r -e modify -e move -e create -e delete ./ | while read line; do
+# inotifywait --excludei=bin|\.git -q -m -r -e modify -e move -e create -e delete ./ | while read line; do
+inotifywait -q -m -r -e modify -e move -e create -e delete ./ | while read line; do
+
+  echo "stuff happened"
   NOW=`$TIME_COMMAND`
 
   if [ "$NOW" -gt "$PREVIOUS_RUN_TIME" ]; then
     # this would be much more elegant if express responded to
     # HUP correctly, for my definition of "correct".
 
-    kill -1 $NODE_PID
-    restart_node
+    kill -9 $NODE_PID
+    sleep 0.5
     PREVIOUS_RUN_TIME=`$TIME_COMMAND`
+    restart_node
     echo "reloaded"
   fi
 
--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,12 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
-};
-
-exports.news = function(req, res){
   var xml = require('../lib/rssparser');
 
   xml.rssurl = 'http://blog.collispuro.com/atom.xml';
 
   xml.items(function(feedItems){
-    res.render('news', { title: 'news', feed_items: feedItems });
+    res.render('index', { title: 'news', feed_items: feedItems });
   });
 };
+
